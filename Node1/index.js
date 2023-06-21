60b36198b7a2a68705e2afd694a2886ffe11515d
ritesh22201 <riteshgoswami22201@gmail.com>
2023-06-21 23:18:58 +0530

    code added

--- Node1/index.js
+++ Node1/index.js
@@ -1,4 +1,22 @@
-const {sum, diff, mul, div} = require('./data');
+// const {sum, diff, mul, div} = require('./data');
 
-console.log(sum(4, 3));
-console.log(mul(4, 3));
+// console.log(sum(4, 3));
+// console.log(mul(4, 3));
+
+const fs = require('fs');
+
+const data = fs.readFileSync('./text.txt', 'utf-8')
+    // if(err){
+    //     console.log(err)
+    // }
+    // else{
+    //     console.log(data)
+    // }
+    console.log(data);
+    console.log('Hello');
+
+    fs.writeFileSync('./text.txt', 'Hello Guys', (err) => {
+        err ? console.log(err) : console.log('done');
+    });
+
+
