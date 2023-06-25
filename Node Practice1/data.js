0d48aca46021f249d76db99ea0ef7439c978c9a6
ritesh22201 <riteshgoswami22201@gmail.com>
2023-06-25 10:33:22 +0530

    code added

---
+++ Node Practice1/data.js
@@ -0,0 +1,18 @@
+
+function sum (a, b) {
+    return a + b;
+}
+
+function diff (a, b) {
+    return a - b;
+}
+
+function mult (a, b) {
+    return a * b;
+}
+
+function div (a, b) {
+    return a / b;
+}
+
+module.exports = {sum, diff, mult, div};
---
+++ Node Practice1/index.js
@@ -0,0 +1,6 @@
+const {sum, diff, mult, div} = require('./data.js');
+
+console.log(sum(10, 10));
+console.log(diff(10, 10));
+console.log(mult(10, 10));
+console.log(div(10, 10));
