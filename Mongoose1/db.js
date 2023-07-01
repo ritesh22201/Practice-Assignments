5a9cd311ec1d1d21fd0d2ec64a8e137cadc66a87
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-01 23:46:57 +0530

    code added

--- Mongoose1/db.js
+++ Mongoose1/db.js
@@ -4,15 +4,14 @@ const main = async() => {
     try {
         const connection = await mongoose.connect('mongodb://127.0.0.1:27017/backend');
         console.log('Connected to DB');
-        // await StudentModel.insertMany({name : 'Chunnu', org : 'Meta', city : 'Delhi'})
+        // await StudentModel.insertMany({name : 'Chunnu', org : 'Meta', city : 'Delhi'})  // For inserting the data
         const student = new StudentModel({
             name : 'Munnu',
             org : 'Google',
             city : 'California'
-        })
-
+        })  // Alternative approach for inserting the data
         await student.save();
-        
+        // const students = await StudentModel.find({$and : [{age : {$gte : 20}}, {age : {$lte : 30}}]});
         connection.disconnect();
         console.log('Disconnected');
     } catch (error) {
@@ -25,9 +24,9 @@ main();
 // Creating structure of the data that I want to store in the data base.
 
 const studentSchema = mongoose.Schema({
-    name : String,
-    age : Number,
-    city : String
+    name : {type : String, required: true},
+    age : {type : Number, required : true},
+    city : {type : String, required : true}
 }, {
     versionKey : false
 })
