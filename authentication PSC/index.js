ee10600d40c667654fa00c55f64df56f6e3d5c6d
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-09 13:18:12 +0530

    code added

--- authentication PSC/index.js
+++ authentication PSC/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connection = require('./db');
 const userRouter = require('./routes/user.route');
+const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 app.use('/users', userRouter);
@@ -9,7 +10,20 @@ app.get('/', (req, res) => {
     res.send('Homepage');
 })
 
-app.listen(8080, async() => {
+app.get('/todos', (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+
+    jwt.verify(token, 'masai', (err, decoded) => {
+        if (decoded) {
+            res.status(200).send({'msg' : 'The todos are here!!'});
+        }
+        else{
+            res.status(400).send('Invalid token!!');
+        }
+    })
+})
+
+app.listen(8080, async () => {
     try {
         await connection;
         console.log('Connected to DB');
--- authentication PSC/routes/user.route.js
+++ authentication PSC/routes/user.route.js
@@ -1,26 +1,28 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const UserModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 
 const userRouter = Router();
 
-userRouter.post('/register', async(req, res) => {
+userRouter.post('/register', async (req, res) => {
     try {
         const user = await UserModel.create(req.body);
-        res.status(200).send({'msg' : 'Registration Successful', user});
+        res.status(200).send({ 'msg': 'Registration Successful', user });
     } catch (error) {
         res.status(400).send(error.message);
     }
 })
 
-userRouter.post('/login', async(req, res) => {
-    const {email, password} = req.body;
+userRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
     try {
-        const user = await UserModel.findOne({email, password});
-        if(user){
-            res.status(200).send({'msg' : 'Login Successful', user});
+        const user = await UserModel.findOne({ email, password });
+        if (user) {
+            const token = jwt.sign({ course: 'backend' }, 'masai');
+            res.status(200).send({ 'msg': 'Login Successful', token });
         }
-        else{
-        res.status(400).send({'msg' : 'Wrong Credentials'});
+        else {
+            res.status(400).send({ 'msg': 'Wrong Credentials' });
         }
     } catch (error) {
         res.status(400).send(error.message);
