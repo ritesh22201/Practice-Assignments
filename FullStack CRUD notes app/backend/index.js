ab5fdec75caa51b39bc1b46445b9c805153edbee
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-11 16:53:31 +0530

    code added

--- FullStack CRUD notes app/backend/index.js
+++ FullStack CRUD notes app/backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const connection = require('./db');
+const userRouter = require('./routes/User.router');
 const app = express();
 app.use(express.json());
-
+app.use('/users', userRouter);
 
 
 app.listen(8080, async() => {
--- FullStack CRUD notes app/backend/routes/User.router.js
+++ FullStack CRUD notes app/backend/routes/User.router.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const UserModel = require('../models/User.model');
 const userRouter = express.Router();
 
 
 userRouter.post('/register', async(req, res) => {
     try {
-        
+       const user = await UserModel.create(req.body);
+       res.status(200).send({'msg' : 'User has been registered', user});
     } catch (error) {
-        
+        res.status(400).send(error.message);
     }
 })
+
+userRouter.post('/login', async(req, res) => {
+    const {email, pass} = req.body;
+    try {
+        const user = await UserModel.findOne({email, pass});
+        
+        if(user){
+            res.status(200).send({'msg' : 'User logged in successfully', token : ''})
+        }
+        else{
+            res.status(400).send({'msg' : 'Wrong credentials'})
+        }
+    } catch (error) {
+        res.status(400).send({'msg' : error.message });
+    }
+})
+
+module.exports = userRouter;
